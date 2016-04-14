7a6f12123e25113b088b75e0730d0d2199ea6e29
Macartur Sousa <macartur.sc@gmail.com>
2016-04-14 09:50:34 -0300

    Adding elasticsearch plugin
    Signed-off-by: Macartur Sousa <macartur.sc@gmail.com>

---
+++ plugins/elasticsearch/lib/elasticsearch_plugin.rb
@@ -0,0 +1,13 @@
+class ElasticsearchPlugin < Noosfero::Plugin
+
+  def self.plugin_name
+    # FIXME
+    "ElasticsearchPlugin"
+  end
+
+  def self.plugin_description
+    # FIXME
+    _("A plugin that does this and that.")
+  end
+
+end
