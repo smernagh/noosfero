32950484d4dd549fd32d16667098517bb086d743
Luciano Prestes Cavalcanti <lucianopcbr@gmail.com>
2016-04-19 14:46:22 +0000

    Index models for elasticsearch
    Signed-off-by: Luciano Prestes Cavalcanti <lucianopcbr@gmail.com>
    Signed-off-by: Lucas Moura <lucas.moura128@gmail.com>

--- plugins/elasticsearch/lib/elasticsearch_plugin.rb
+++ plugins/elasticsearch/lib/elasticsearch_plugin.rb
@@ -10,4 +10,36 @@ class ElasticsearchPlugin < Noosfero::Plugin
     _("A plugin that does this and that.")
   end
 
+  Noosfero::Application.class_eval do
+    config.after_initialize do
+      models = ActiveRecord::Base.descendants.select do |model|
+        model.const_defined?("SEARCHABLE_FIELDS")
+      end
+
+      models.each do |model|
+        model.class_eval do
+          include Elasticsearch::Model
+          include Elasticsearch::Model::Callbacks
+
+          settings index: { number_of_shards: 1 } do
+            mappings dynamic: 'false' do
+              model::SEARCHABLE_FIELDS.each do |field, value|
+                indexes field
+              end
+            end
+          model.__elasticsearch__.client.indices.delete \
+            index: model.index_name rescue nil
+          model.__elasticsearch__.client.indices.create \
+            index: model.index_name,
+            body: {
+              settings: model.settings.to_hash,
+              mappings: model.mappings.to_hash
+            }
+
+          model.import
+          end
+        end
+      end
+    end
+  end
 end
