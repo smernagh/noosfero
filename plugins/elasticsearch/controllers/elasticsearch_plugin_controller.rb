99d61d561dff16b048c1f3a0c3c93d9f85fdb87e
Macartur Sousa <macartur.sc@gmail.com>
2016-04-25 16:47:33 -0300

    Make query generic
    Signed-off-by: Macartur Sousa <macartur.sc@gmail.com>

--- plugins/elasticsearch/controllers/elasticsearch_plugin_controller.rb
+++ plugins/elasticsearch/controllers/elasticsearch_plugin_controller.rb
@@ -4,15 +4,37 @@ class ElasticsearchPluginController < ApplicationController
  
   def search
     @results = []
+    @query = params[:q]
+    @checkbox = {}
+    if params[:model].present?
+        params[:model].keys.each do |model|
+            @checkbox[model.to_sym] = true
+            klass = model.classify.constantize
+            query = get_query params[:q], klass
+            @results += klass.__elasticsearch__.search(query).records.to_a
+        end
+    end
 
-    @checkbox = {:articles => params[:articles].present?,
-                 :communities => params[:communities].present?,
-                 :people => params[:people].present?
-                } 
+  end
 
-    @results += Article.__elasticsearch__.search('{}').records.to_a if params[:articles]
-    @results += Community.__elasticsearch__.search('{}').records.to_a if params[:communities]
-    @results += Person.__elasticsearch__.search('{}').records.to_a if params[:people]
+  private
+
+  def get_query text, klass
+    query = {}
+    unless text.blank?
+
+       fields = klass::SEARCHABLE_FIELDS.map {|k, v|  "#{k}^#{v[:weight]}"}
+
+       query = {
+           query: {
+               multi_match: {
+                   query: text,
+                   fields: fields
+               }
+           }
+       }
+    end
+    query
   end
 
 end
