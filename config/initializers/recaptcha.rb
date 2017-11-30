b90b435c11582bdd7179df9fc5283bfe4d92ff93
Rodrigo Souto <rodrigo@colivre.coop.br>
2017-11-30 16:13:31 -0300

    stretch-update: update recaptcha config

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,4 +1,4 @@
 Recaptcha.configure do |config|
-  config.site_key   = '6LcK7CYUAAAAABl-eMkoFIv3VvOOIPwiXeinFmUi'
-  config.secret_key = '6LcK7CYUAAAAAN-oooxubQRkDEf_EKqygh4Hs3CB'
+  config.public_key   = '6LcK7CYUAAAAABl-eMkoFIv3VvOOIPwiXeinFmUi'
+  config.private_key = '6LcK7CYUAAAAAN-oooxubQRkDEf_EKqygh4Hs3CB'
 end
